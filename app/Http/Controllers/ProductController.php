a38125847affd763f8e76ebd0d9aac33b91c62e2
mixxvn <unitonework@gmail.com>
2022-12-17 21:31:15 +0700

    add crud features

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Support\Str;
+use App\Http\Requests\ProductRequest;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return view('pages.product.index')->with([
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.product.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        // $data = new Product;
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->name);
+        // $dataCollection = collect($data);
+        // dd($datas);
+        // $dataCollection->save();
+        Product::create($data);
+
+        return to_route('product.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = Product::findOrFail($id);
+
+        return view('pages.product.edit')->with([
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, $id)
+    {
+        $data = $request->all();
+        $data['slug'] = str::slug($request->name);
+
+        $item = Product::findOrFail($id);
+        $item->update($data);
+        
+        return to_route('product.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Product::findOrFail($id);
+        $data->delete();
+
+        return to_route('product.index');
+    }
+}
